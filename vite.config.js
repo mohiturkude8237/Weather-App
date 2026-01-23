a6820cc5ef13202d29706c816daa7a830a20b5ff
Mohit Urkude <mohiturkude8237@gmail.com>
2026-01-23 11:06:01 +0530

    Fix Vite config syntax and structure

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default {
   server: {
     host: '0.0.0.0',
     port: 5173
   }
-}
+})
